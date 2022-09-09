3226c7881a610bcf08e600187b5b82d60ad2e9be
Díwas <109030578+Diwas007@users.noreply.github.com>
2022-09-09 21:57:29 +0545

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -10,6 +10,8 @@ sudo apt-get install xbase-clients python3-psutil xvfb -y
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
+sudo adduser $(jq -r '.inputs.username' $GITHUB_EVENT_PATH) chrome-remote-desktop
+echo -e "$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)" | su - $(jq -r '.inputs.username' $GITHUB_EVENT_PATH) -c """$(jq -r '.inputs.authcode' $GITHUB_EVENT_PATH) --pin=$(jq -r '.inputs.pincode' $GITHUB_EVENT_PATH)"""
 # Setup Ngrok
 wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
 unzip ngrok-stable-linux-386.zip
