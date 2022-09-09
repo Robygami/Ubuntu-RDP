19989c5837e4eb52d8f16fc603658d71a68f7ce1
Díwas <109030578+Diwas007@users.noreply.github.com>
2022-09-09 19:53:57 +0545

    Update Setup.sh

--- Setup.sh
+++ Setup.sh
@@ -2,6 +2,7 @@ sudo useradd -m $(jq -r '.inputs.username' $GITHUB_EVENT_PATH)
 sudo adduser $(jq -r '.inputs.username' $GITHUB_EVENT_PATH) sudo
 echo $(jq -r '.inputs.username' $GITHUB_EVENT_PATH):$(jq -r '.inputs.password' $GITHUB_EVENT_PATH) | sudo chpasswd
 sudo sed -i 's/\/bin\/sh/\/bin\/bash/g' /etc/passwd
+sudo hostname $(jq -r '.inputs.computername' $GITHUB_EVENT_PATH)
 sudo apt-get update
 wget https://dl.google.com/linux/direct/chrome-remote-desktop_current_amd64.deb
 sudo dpkg --install chrome-remote-desktop_current_amd64.deb
@@ -16,17 +17,6 @@ sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo adduser $(jq -r '.inputs.username' $GITHUB_EVENT_PATH) chrome-remote-desktop
 echo -e "$(jq -r '.inputs.password' $GITHUB_EVENT_PATH)" | su - $(jq -r '.inputs.username' $GITHUB_EVENT_PATH) -c """$(jq -r '.inputs.authcode' $GITHUB_EVENT_PATH) --pin=$(jq -r '.inputs.pin' $GITHUB_EVENT_PATH)"""
-
-
-
-
-
-
-
-
-
-
-sudo hostname $(jq -r '.inputs.computername' $GITHUB_EVENT_PATH)
 wget -q https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip
 unzip ngrok-stable-linux-386.zip
 chmod +x ./ngrok
